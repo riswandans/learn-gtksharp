6c9500384cdd081df36bdbc365f189672a9a95d1
root <root.devilscream@gmail.com>
2016-04-11 21:08:38 +0800

    Add if example

---
+++ If/example/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Gtk;
+
+namespace example
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Application.Init ();
+			MainWindow win = new MainWindow ();
+			int test;
+			test = 1000;
+			if (test > 900) {
+				win.Title = "High";
+			} else {
+				win.Title = "Low";
+			}
+			win.Show ();
+			Application.Run ();
+		}
+	}
+}
