6821dcb84dfba72b662968b4a0c98c18aa49db2e
root <root.devilscream@gmail.com>
2016-04-11 17:54:31 +0800

    Add demo variable

---
+++ Variable/Example/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Gtk;
+
+namespace Example
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Application.Init ();
+			MainWindow win = new MainWindow ();
+			string title;
+			title = "Your Title";
+			win.Title = title;
+			win.Show ();
+			Application.Run ();
+		}
+	}
+}
